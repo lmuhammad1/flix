2df33bc02b5ddc851cdded9691a50810bada8b9c
Louis Muhammad <louis.muhammad@gmail.com>
2014-06-08 07:56:31 -0400

    Added username to users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -44,5 +44,31 @@ describe UsersController do
     
   end
   
+  context "when signed in as the wrong user" do
     
+    before do
+      @wrong_user = User.create!(user_attributes(email: "wrong@example.com", username: "wronguser"))
+      session[:user_id] = @wrong_user.id
+    end
+    
+    it "cannot edit another user" do
+      get :edit, id: @user
+      
+      expect(response).to redirect_to(root_url)
+    end
+    
+    it "cannot update another user" do
+      patch :update, id: @user
+      
+      expect(response).to redirect_to(root_url)
+    end
+    
+    it "cannot destroy another user" do
+      delete :destroy, id: @user
+      
+      expect(response).to redirect_to(root_url)
+    end
+    
+  end
+  
 end
